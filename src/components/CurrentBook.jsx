38693f2661395857010db8958826c4ada662abd4
ChristiHarlow <103293099+ChristiHarlow@users.noreply.github.com>
2025-10-09 15:31:58 -0400

    Update CurrentBook.jsx

--- src/components/CurrentBook.jsx
+++ src/components/CurrentBook.jsx
@@ -3,7 +3,7 @@ import BookCard from "./BookCard";
 export default function CurrentBook() {
   // Replace with your real current pick
   const current = {
-    title: "The Man the Moment Demands",
+    title: "The Man the Moment Demands: Master the 10 Characteristics of the Comprehensive Man",
     author: "Jason Wilson",
     cover: "/images/books/jasonWilson.png",
     link: "https://www.amazon.com/Man-Moment-Demands-Characteristics-Comprehensive/dp/1400249058/ref=tmm_pap_swatch_0",
