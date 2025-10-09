5b74d057f43a746dacc0525b2fe36f0f92f0fe01
ChristiHarlow <103293099+ChristiHarlow@users.noreply.github.com>
2025-10-08 21:18:31 -0400

    Books

--- src/components/UpcomingBook.jsx
+++ src/components/UpcomingBook.jsx
@@ -3,12 +3,12 @@ import BookCard from "./BookCard";
 export default function UpcomingBook() {
   // Swap with your chosen upcoming title
   const nextUp = {
-    title: "The Creative Act",
-    author: "Rick Rubin",
-    cover: "/images/books/creativeAct.png", // put this file in public/images/books/
-    price: "$21.99",
-    link: "https://www.amazon.com/dp/0593652886",
-    description: "A reflective guide to making, sharing, and living creatively."
+    title: "107 Days",
+    author: "Kamala Harris",
+    cover: "/images/books/kamalaHarris.png", // put this file in public/images/books/
+    price: "$20.98",
+    link: "https://www.amazon.com/107-Days-Kamala-Harris-ebook/dp/B0F6Z1MSG5?ref_=ast_author_mpb",
+    description: "For the first time, and with surprising and revealing insights, former Vice President Kamala Harris tells the story of one of the wildest and most consequential presidential campaigns in American history.."
   };
 
   return (
