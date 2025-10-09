2c7ff88f31990321962c40a495b56f0ddbc7112a
ChristiHarlow <103293099+ChristiHarlow@users.noreply.github.com>
2025-10-08 21:35:55 -0400

    Book

--- src/components/UpcomingBook.jsx
+++ src/components/UpcomingBook.jsx
@@ -6,8 +6,7 @@ export default function UpcomingBook() {
     title: "107 Days",
     author: "Kamala Harris",
     cover: "/images/books/kamalaHarris.png", // put this file in public/images/books/
-    price: "$20.98",
-    link: "https://www.amazon.com/107-Days-Kamala-Harris-ebook/dp/B0F6Z1MSG5?ref_=ast_author_mpb",
+        link: "https://www.amazon.com/107-Days-Kamala-Harris-ebook/dp/B0F6Z1MSG5?ref_=ast_author_mpb",
     description: "For the first time, and with surprising and revealing insights, former Vice President Kamala Harris tells the story of one of the wildest and most consequential presidential campaigns in American history.."
   };
 
