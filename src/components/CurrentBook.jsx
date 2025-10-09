f3b60fc1c7f2d13c88928c3ff6a7d730fc021a42
ChristiHarlow <103293099+ChristiHarlow@users.noreply.github.com>
2025-10-08 21:23:16 -0400

    Update CurrentBook.jsx

--- src/components/CurrentBook.jsx
+++ src/components/CurrentBook.jsx
@@ -6,7 +6,6 @@ export default function CurrentBook() {
     title: "The Man the Moment Demands",
     author: "Jason Wilson",
     cover: "/images/books/jasonWilson.png",
-    price: "$10.00",
     link: "https://www.amazon.com/Man-Moment-Demands-Characteristics-Comprehensive/dp/1400249058/ref=tmm_pap_swatch_0",
     description: "Transform your life when you discover the essence of true masculinity and become the right kind of man for every challenge that lies ahead."
   };
